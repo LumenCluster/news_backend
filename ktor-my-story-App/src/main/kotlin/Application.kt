316a67ef72776d4e162f1c1a82b863ce2ff35f48
Noor-ulain555 <145388440+Noor-ulain555@users.noreply.github.com>
2025-04-23 00:51:12 +0500

    Update Application.kt

--- ktor-my-story-App/src/main/kotlin/Application.kt
+++ ktor-my-story-App/src/main/kotlin/Application.kt
@@ -6,9 +6,13 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0",
-        module = Application::module).start(wait = true)
+    embeddedServer(Netty, port = System.getenv("PORT")?.toInt() ?: 8080, host = "0.0.0.0",
+                   module = Application::module).start(wait = true)
+
+    // embeddedServer(Netty, port = 8080, host = "0.0.0.0",
+    //     module = Application::module).start(wait = true)
 }
+
 
 fun Application.module() {
     DatabaseFactory.init()
