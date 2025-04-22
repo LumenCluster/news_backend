0297e911580c05bac3f11b478a1b0f22a1f746da
Noor-ulain555 <145388440+Noor-ulain555@users.noreply.github.com>
2025-04-22 23:56:54 +0500

    Update DatabaseFactory.kt

--- ktor-my-story-App/src/main/kotlin/DatabaseFactory.kt
+++ ktor-my-story-App/src/main/kotlin/DatabaseFactory.kt
@@ -6,12 +6,19 @@ import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseFactory {
     fun init() {
-        val config = HikariConfig().apply {
-            jdbcUrl = "jdbc:postgresql://localhost:5432/test"
+        // val config = HikariConfig().apply {
+        //     jdbcUrl = "jdbc:postgresql://localhost:5432/test"
+        //     driverClassName = "org.postgresql.Driver"
+        //     username = "postgres"
+        //     password = "noor5"
+        //     maximumPoolSize = 5
+        // }
+          val config = HikariConfig().apply {
+            jdbcUrl = "jdbc:postgresql://ktor_news_app_db_user:HHe0fYdklak32jnFrN2Ce7s16BKCnRjD@dpg-d03u2649c44c738aupmg-a.frankfurt-postgres.render.com/ktor_news_app_db"
             driverClassName = "org.postgresql.Driver"
-            username = "postgres"
-            password = "noor5"
-            maximumPoolSize = 5
+            username = "ktor_news_app_db_user"
+            password = "HHe0fYdklak32jnFrN2Ce7s16BKCnRjD"
+            maximumPoolSize = 8
         }
         val dataSource = HikariDataSource(config)
         Database.connect(dataSource)
