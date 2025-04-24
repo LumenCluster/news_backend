9c094c45bbf62734ec4750d97ed8fee4eb733c56
Noor-ulain555 <145388440+Noor-ulain555@users.noreply.github.com>
2025-04-24 19:03:14 +0500

    Update DatabaseFactory.kt

--- ktor-my-story-App/src/main/kotlin/DatabaseFactory.kt
+++ ktor-my-story-App/src/main/kotlin/DatabaseFactory.kt
@@ -13,13 +13,20 @@ object DatabaseFactory {
         //     password = "noor5"
         //     maximumPoolSize = 5
         // }
-          val config = HikariConfig().apply {
-            jdbcUrl = "jdbc:postgresql://ktor_news_app_db_user:HHe0fYdklak32jnFrN2Ce7s16BKCnRjD@dpg-d03u2649c44c738aupmg-a.frankfurt-postgres.render.com/ktor_news_app_db"
-            driverClassName = "org.postgresql.Driver"
-            username = "ktor_news_app_db_user"
-            password = "HHe0fYdklak32jnFrN2Ce7s16BKCnRjD"
-            maximumPoolSize = 8
-        }
+        //   val config = HikariConfig().apply {
+        //     jdbcUrl = "jdbc:postgresql://ktor_news_app_db_user:HHe0fYdklak32jnFrN2Ce7s16BKCnRjD@dpg-d03u2649c44c738aupmg-a.frankfurt-postgres.render.com/ktor_news_app_db"
+        //     driverClassName = "org.postgresql.Driver"
+        //     username = "ktor_news_app_db_user"
+        //     password = "HHe0fYdklak32jnFrN2Ce7s16BKCnRjD"
+        //     maximumPoolSize = 8
+        // }
+        val config = HikariConfig().apply {
+    jdbcUrl = "jdbc:postgresql://dpg-d03u2649c44c738aupmg-a.frankfurt-postgres.render.com:5432/ktor_news_app_db"
+    driverClassName = "org.postgresql.Driver"
+    username = System.getenv("DB_USER")
+    password = System.getenv("DB_PASSWORD")
+    maximumPoolSize = 8
+}
         val dataSource = HikariDataSource(config)
         Database.connect(dataSource)
         transaction {
