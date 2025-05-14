d9a619f49fd8762070efff2631c9707e15b2bd69
Noor-ulain555 <145388440+Noor-ulain555@users.noreply.github.com>
2025-05-14 18:10:51 +0500

    Create Notifications.kt

---
+++ ktor-my-story-App/src/main/kotlin/Notifications.kt
@@ -0,0 +1,14 @@
+package com.example.ApplicationKt
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+
+
+object Notifications : IntIdTable() {
+
+    val title = varchar("title", 255)
+    val name = varchar("name", 255) // category name
+    val imageUrl = text("image_url").nullable()
+    val author = varchar("author", 255)
+    val isRead = bool("is_read").default(false)
+    val createdAt = long("created_at")
+}
