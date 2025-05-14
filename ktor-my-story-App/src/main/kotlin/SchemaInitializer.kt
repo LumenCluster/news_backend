6effbbe7b79845279f520c06fd5e85e4489c70c6
Noor-ulain555 <145388440+Noor-ulain555@users.noreply.github.com>
2025-05-14 17:07:57 +0500

    Update SchemaInitializer.kt

--- ktor-my-story-App/src/main/kotlin/SchemaInitializer.kt
+++ ktor-my-story-App/src/main/kotlin/SchemaInitializer.kt
@@ -8,6 +8,8 @@ import org.jetbrains.exposed.sql.transactions.transaction
             transaction {
                 SchemaUtils.create(Categories)
                 SchemaUtils.create(Articles)
+                SchemaUtils.create(Notifications) 
+
 
             }
         }
