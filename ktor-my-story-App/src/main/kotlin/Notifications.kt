e12fb5f265ce6ef01d6f87cf62a461c6a7bed9e9
Noor-ulain555 <145388440+Noor-ulain555@users.noreply.github.com>
2025-06-12 21:16:22 +0500

    Update Notifications.kt

--- ktor-my-story-App/src/main/kotlin/Notifications.kt
+++ ktor-my-story-App/src/main/kotlin/Notifications.kt
@@ -6,7 +6,7 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 object Notifications : IntIdTable() {
 
     val title = varchar("title", 255)
-    val name = varchar("name", 255) // category name
+    val name = varchar("name", 255) //  name for category
     val imageUrl = text("image_url").nullable()
     val author = varchar("author", 255)
     val isRead = bool("is_read").default(false)
