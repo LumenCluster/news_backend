f57b9cdcb36b289658cbfa810c987f1bf82c2aa3
Noor-ulain555 <145388440+Noor-ulain555@users.noreply.github.com>
2025-11-25 19:23:38 +0500

    Update build.gradle.kts

--- ktor-my-story-App/build.gradle.kts
+++ ktor-my-story-App/build.gradle.kts
@@ -52,6 +52,10 @@ dependencies {
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 
+    implementation("io.ktor:ktor-client-core:3.1.3")
+    implementation("io.ktor:ktor-client-cio:3.1.3")
+    implementation("io.ktor:ktor-client-content-negotiation:3.1.3")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.1.3")
 
 
 
